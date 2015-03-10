eaf649feca6f05f1d192216b7d4f7739a5b5d737
JLChnToZ <jeremylam619@gmail.com>
2015-03-11 00:37:19 +0800

    Update the support version.

--- index.js
+++ index.js
@@ -12,13 +12,13 @@
     }
   ];
 
-  bili.parse = function(postContent, callback) {
+  bili.parse = function(data, callback) {
     try {
       for(var i = 0; i < converts.length; i++)
-        postContent = postContent.replace(converts[i].from, converts[i].to);
-      callback(null, postContent);
+        data.postData.content = data.postData.content.replace(converts[i].from, converts[i].to);
+      callback(null, data);
     } catch(ex) {
-      callback(ex, postContent);
+      callback(ex, data);
     }
   };
 
