7a4d3a87a790abf38013c2b43439f73e2e16d511
R <3413343734@qq.com>
2019-07-30 14:50:47 +0800

    [feat]: 支持 b23 短链接 (#13)

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-(function(bili) {
+; (function (bili) {
 	"use strict";
 	var converts = [
 		{ // Video
 			from: /<a href="(?:https?:\/\/)?(?:www\.)?bilibili\.(?:tv|com)\/video\/av(\d+).*?">.+<\/a>/g,
-			to: '<div class="embed-responsive embed-responsive-16by9">'+
-			'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?aid=$1&as_wide=1" '+
-			'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
+			to: '<div class="embed-responsive embed-responsive-16by9">' +
+				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?aid=$1&as_wide=1" ' +
+				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
+		},
+		{
+			// b23 video (short url)
+			// eg: https://b23.tv/av*****
+			from: /<a href="(?:https?:\/\/)?(?:www\.)?b23\.(?:tv|com)\/av(\d+).*?">.+<\/a>/g,
+			to: '<div class="embed-responsive embed-responsive-16by9">' +
+				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?aid=$1&as_wide=1" ' +
+				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
 		}
 	];
 
-	bili.parse = function(data, callback) {
-		//console.log(data);
+	bili.parse = function (data, callback) {
 		try {
-			for(var i = 0; i < converts.length; i++)
+			for (var i = 0; i < converts.length; i++)
 				data.postData.content = data.postData.content.replace(converts[i].from, converts[i].to);
 			callback(null, data);
-		} catch(ex) {
+		} catch (ex) {
 			callback(ex, data);
 		}
 	};
 
-	bili.addScripts = function (scripts, callback){
-		//console.log("o.o...");
+	bili.addScripts = function (scripts, callback) {
 		scripts.push('/assets/src/bilibili.js');
-		callback(null,scripts);
+		callback(null, scripts);
 	}
 })(module.exports);
